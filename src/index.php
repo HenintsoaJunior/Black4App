5501f45b6e9ca24696ba164070980742ba1f1381
Daxido junior <143500070+HenintsoaJunior@users.noreply.github.com>
2024-10-05 17:00:13 +0300

    black4App

--- src/index.php
+++ src/index.php
@@ -1,1 +1,24 @@
-<?php echo "Hello zandry"; ?>
+<?php 
+require 'vendor/autoload.php';
+
+ParseClient::initialize( "jHL80GULkIzJwlyKS5aatZuDqhzmPUrMYAzeKZTO", "6Qc3Er8HAYkQzhMHzbQLwDgSFiAwq1OcYeDEu7gv", "srRloTo8IGu4YlxFK5TCO3A2F5g9rduGkxWBCxjl" );
+ParseClient::setServerURL('https://parseapi.back4app.com', '/');
+
+use Parse\ParseException;
+use Parse\ParseObject;
+
+$myCustomObject = new ParseObject("Orders");
+
+$myCustomObject->set("order_id", "1");
+$myCustomObject->set("item_name", "dax");
+$myCustomObject->set("name", "dax");
+
+try {
+  $myCustomObject->save();
+  echo 'New object created with objectId: ' . $myCustomObject->getObjectId();
+} catch (ParseException $ex) {
+  // Execute any logic that should take place if the save fails.
+  // error is a ParseException object with an error code and message.
+  echo 'Failed to create new object, with error message: ' . $ex->getMessage();
+}
+?>
